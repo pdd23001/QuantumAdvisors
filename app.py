c9166ff22b4595a71087274c0b4c1bd4b11b0ac1
kss22006 <krish.sawant@uconn.edu>
2025-03-01 03:15:49 -0500

    new frontend

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/quantum")
 def quantum():
     return render_template("quantum.html")
 
+
 @app.route("/advisor")
 def advisor():
+    print()
     return render_template("advisor.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
